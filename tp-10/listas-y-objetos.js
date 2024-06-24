67e6d2259a1a3a937adf2ba5dce94d45464bbb6e
vikkybl <164422579+vikkybl@users.noreply.github.com>
2024-06-24 20:52:26 -0300

    Update listas-y-objetos.js

--- tp-10/listas-y-objetos.js
+++ tp-10/listas-y-objetos.js
@@ -1,4 +1,4 @@
-var listaPersonasEjemplo = [
+let listaPersonasEjemplo = [
     {
         "apellido": "Perez",
         "nombre": "Juan",
@@ -35,8 +35,12 @@ var listaPersonasEjemplo = [
  * - el mismo listado, ordenado alfabéticamente por el apellido de la persona 
  */
 function ordenarPorApellido(listaDePersonas) {
-    // 
+    let resultado = listaDePersonas.sort((a,b) => { 
+        return a.apellido.localeCompare(b.apellido);
+    });
+    return resultado;
 }
+
 console.log("ordenarPorApellido()", ordenarPorApellido(listaPersonasEjemplo));
 
 /**
@@ -49,8 +53,9 @@ console.log("ordenarPorApellido()", ordenarPorApellido(listaPersonasEjemplo));
  * - una lista de strings, con sólo los nombres de las personas
  */
 function soloNombres(listaDePersonas) {
-    // 
+    return listaDePersonas.map(persona => persona.nombre);
 }
+
 console.log("soloNombres()", soloNombres(listaPersonasEjemplo));
 
 /**
@@ -63,7 +68,8 @@ console.log("soloNombres()", soloNombres(listaPersonasEjemplo));
  * - un numero, con el cálculo del promedio de las edades
  */
 function promedioEdades(listaDePersonas) {
-    //
+    const totalEdades = listaDePersonas.reduce((suma, persona) => suma + persona.edad, 0);
+    return totalEdades / listaDePersonas.longitud;
 }
 console.log("promedioEdades()", promedioEdades(listaPersonasEjemplo));
 
@@ -77,7 +83,7 @@ console.log("promedioEdades()", promedioEdades(listaPersonasEjemplo));
  * - una lista, array, conteniendo solamente las personas con más de 18 años
  */
 function soloMayoresDeEdad(listaDePersonas) {
-    //
+    return listaDePersonas.filter(persona => persona.edad > 18);
 }
 console.log("soloMayoresDeEdad()", soloMayoresDeEdad(listaPersonasEjemplo));
 
@@ -91,6 +97,6 @@ console.log("soloMayoresDeEdad()", soloMayoresDeEdad(listaPersonasEjemplo));
  * - una objeto con la persona de mayor edad en todo el listado. En caso de que hayan 2 personas con la misma edad, se puede retornar la primera que aparezca en el listado.
  */
 function laPersonaMayor(listaDePersonas) {
-    //
+    return listaDePersonas.reduce((mayor, persona) => persona.edad > mayor.edad ? persona : mayor, listaDePersonas[0]);
 }
 console.log("laPersonaMayor()", laPersonaMayor(listaPersonasEjemplo));
